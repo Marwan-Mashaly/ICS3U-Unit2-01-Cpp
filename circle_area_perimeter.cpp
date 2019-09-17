3a0072e7e7ea6c62f2b36038cc79db2d7d756c4e
Ubuntu <ubuntu@ip-172-31-30-160.ec2.internal>
2019-09-17 00:07:04 +0000

    added circle's area and perimeter program

---
+++ circle_area_perimeter.cpp
@@ -0,0 +1,16 @@
+// copyright (c) 2019 Marwan Mashaly All rights reserved
+//
+// Created by: Marwan Mashaly
+// Created on: September 2019
+// This program calculates the Area and Perimeter of a cricle
+//    with the Radius of 15mm
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "if a rectangle has the radius of 15mm:" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << (M_PI*pow(15.0, 2.0)) << "mm^2" << std::endl;
+    std::cout << "perimeter is " << (2*M_PI*15) << "mm" << std::endl;
+}
